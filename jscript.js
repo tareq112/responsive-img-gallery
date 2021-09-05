28f9f2a3f1bbbb385d1c366edd625c5b09bb8b61
mollah_tareq <tareqcseuiu@gmail.com>
2021-09-05 21:19:26 +0600

    first part done

--- jscript.js
+++ jscript.js
@@ -1,58 +1,79 @@
+
+//https://picsum.photos/
 const linkup = document.querySelector('#linkbtn');
 const upfrompc = document.querySelector('#fileup');
 const show = document.querySelector('.container');
+
+
+
+
 
 //-----img collect from link--------
 
 function srcCollect(e) {
     let link = document.getElementById('url').value;
-    console.log("userlink:" + link);
+
     getMeta(link);
+   
+    document.getElementById('url').value='';
 }
 //---------------get img height width----------------
 
 function getMeta(url) {
 
-    var img = new Image();
-      img.src = url;
+    let img = new Image();
+    img.src = url;
     img.onload = function () {
-        show.innerHTML += `<div class="item">
+        show.innerHTML += `<div class="item" id="zoom">
             <img src="${img.src}" alt="uploaded img" width="${this.width}" height="${this.height}">
+            <button id="btn">Delete</button>
             </div>`;
-        console.log("inner html:" + show.innerHTML);
-    }
-  
 
+    }
+    
+    
 };
 linkup.addEventListener('click', srcCollect);
 
+/*------upload Item  from  pc-----*/
 
-
-
-
-
-
-
-//-----img upload  from pc--------
-
-/*function imgCollect(e) {
-    const pcimg = document.getElementById('pc');
-    console.log(pcimg);
-    show.innerHTML+= `<div class="item">
-    <img src="${e.src}" alt="uploaded img" width="${this.width}" height="${this.height}">
-    </div>`;
-}
-upfrompc.addEventListener('click', imgCollect);*/
-
-function readURL(input){
-    if(input.files && input.files[0]){
+function readURL(input) {
+    if (input.files && input.files[0]) {
         let reader = new FileReader();
-        reader.onload = function(e){
-            console.log(e);
-            show.innerHTML+= `<div class="item">
+        reader.onload = function (e) {
+
+            show.innerHTML += `<div class="item" id="zoom">
             <img src="${e.target.result}" alt="uploaded img" width="${this.width}" height="${this.height}">
-                         </div>`;
+            <button id="btn">Delete</button>
+            </div>`;
         }
         reader.readAsDataURL(input.files[0]);
     }
+    document.getElementById('pc').value='';
 }
+
+/*------Delete Item-----*/
+
+let dltItem = document.querySelector('.container .btn');
+
+
+ window.addEventListener('click',function(e){
+     if(e.target.id == 'btn'){
+        console.log(e.target);
+        let l = e.target;
+        let p = l.parentElement;
+        p.remove();
+     }
+    
+   
+
+});
+
+
+
+
+    
+
+
+
+
